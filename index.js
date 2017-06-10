37d4171b6548d089b6bca432ede4f453a09f4c12
AkshayaKL <achu4008@yahoo.com>
2017-06-10 11:11:04 +0530

    Adding a constraint and game over conditions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
        var ctx=canvas.getContext("2d");
        var audio=document.getElementById("audio");
        audio.play(); 
+       alert("Read instructions on the right hand side before proceeding");
        var arrowX=20;
        var arrowY=50;
        var arrowLength=25;
@@ -22,13 +23,14 @@
        var malex=80;
        var maley=10;   
        var Paused=false; 
+       var gamecount=0;//declaring variables
       drawbullseye();
       drawarrow();
       document.addEventListener("keydown",shoot,false);
-      document.getElementById("score").innerHTML=score;
+      document.getElementById("score").innerHTML=score;//getting a place to store score
       document.addEventListener("keydown",moveup,false);
       document.addEventListener("keydown",movedown,false);
-      document.addEventListener("keydown",gamepause,false);
+      document.addEventListener("keydown",gamepause,false);//adding Event Listeners for keyboard activities
       function drawarrow()
        {
   
@@ -42,7 +44,7 @@
             ctx.fill();
             ctx.stroke();
             ctx.closePath();
-           }
+           }//this is to draw the arrow in positions desired
       function drawbullseye()
        {
             ctx.beginPath();
@@ -70,7 +72,7 @@
              ctx.arc(ccx,ccy,3,0,Math.PI*2,true);
              ctx.fill();
              ctx.fillStyle="blue";
-             }
+             }//this is to draw the target in positions desired
          function game()
 
         { audio.play();
@@ -83,6 +85,7 @@
                  ctx.drawImage(img,ax,ay,30,30);}
                     
                    if(played)      {
+                                   
                                  
                                  drawman();
                                  drawbullseye();
@@ -101,12 +104,21 @@
                    drawarrow();
                    played=false;
                         arrowX=20;
-                           
+                      gamecount++;     
                      }
 
-                 
+                                     if(((Math.abs(ccx-arrowX-arrowLength)<15)&&(Math.abs(ccy-arrowY)<15))&&((Math.abs(ccx-arrowX-arrowLength)>5)&&(Math.abs(ccy-arrowY)>5)))
+                     {score=score+2;
+                        document.getElementById("targetz").innerHTML="<b>KEEP TRYING HARDER</b>";
+                           
+                       }
+                else  if(((Math.abs(ccx-arrowX-arrowLength)<5)&&(Math.abs(ccy-arrowY)<5))&&((Math.abs(ccx-arrowX-arrowLength)>=3)&&(Math.abs(ccy-arrowY)>=3)))
+                       { score=score+5;
+                           document.getElementById("targetz").innerHTML="<b>Ouch!Nearly Missed It</b>"; 
+                            }
+
                               
-                  if((Math.abs(ccx-arrowX-arrowLength)<=3)&&(Math.abs(ccy-arrowY)<=3))
+                  else if((Math.abs(ccx-arrowX-arrowLength)<=3)&&(Math.abs(ccy-arrowY)<=3))
                        {
                           score=score+10;
                            document.getElementById("targetz").innerHTML="<b>hurray!<b>"; 
@@ -114,15 +126,7 @@
                         }      
                            
                         
-                   else  if(((Math.abs(ccx-arrowX-arrowLength)<5)&&(Math.abs(ccy-arrowY)<5))&&((Math.abs(ccx-arrowX-arrowLength)>=3)&&(Math.abs(ccy-arrowY)>=3)))
-                       { score=score+5;
-                           document.getElementById("targetz").innerHTML="<b>Ouch!Nearly Missed It</b>"; 
-                            }
-                   else if(((Math.abs(ccx-arrowX-arrowLength)<15)&&(Math.abs(ccy-arrowY)<15))&&((Math.abs(ccx-arrowX-arrowLength)>5)&&(Math.abs(ccy-arrowY)>5)))
-                     {score=score+2;
-                        document.getElementById("targetz").innerHTML="<b>KEEP TRYING HARDER</b>";
-                           
-                       }
+                   
                      if((Math.abs(heartx-arrowX-arrowLength)<5)&&(Math.abs(hearty-arrowY)<5))
                        img=document.getElementById("farcher");
                        if((Math.abs(malex-arrowX-arrowLength)<5)&&(Math.abs(maley-arrowY)<5))
@@ -130,80 +134,80 @@
 
 
                      document.getElementById("score").innerHTML=score;
-                                     
+                      document.getElementById("arrowsused" ).innerHTML=gamecount;              
                            
-                      }                     
+                      }
+                   if(gamecount==10) 
+                      {clearInterval(interval);
+                       document.getElementById("targetz").innerHTML="<b>GAME OVER</b>"+"<br />"+"Score="+score}                                                  
              
    
-                  }
+                  }//this is where the actual gaming happens
 
 
 
 
 
-function shoot(e)
-{
-  if(e.keyCode===32)
-    played=true;
-}
-function moveup(e)
-{
-  if(e.keyCode===38)
-{if((arrowY>=0)&&(arrowX==20))
+             function shoot(e)
+             {
+               if(e.keyCode===32)
+                 played=true;
+               }//to shoot out arrows reading spacebar
+             function moveup(e)
+               {
+                 if(e.keyCode===38)
+                {if((arrowY>=0)&&(arrowX==20)&&(gamecount!=10))
   
- 
-
-{
-arrowY-=2;
-ay=arrowY-10;
-ctx.drawImage(img,ax,ay,30,30);
-}
-}
+                    {
+                        arrowY-=2;
+                        ay=arrowY-10;
+                       ctx.drawImage(img,ax,ay,30,30);
+                    }
+                }                  
   
-}
-function movedown(e)
-{
-if(e.keyCode===40)
-{if((arrowY<=canvas.height)&&(arrowX==20))
+              }
+              function movedown(e)
+             {
+                 if(e.keyCode===40)
+          {if((arrowY<=canvas.height)&&(arrowX==20)&&(gamecount!=10))
 
 
-{
-arrowY+=2;
+                  {
+                   arrowY+=2;
 
- ay=arrowY-10;
- ctx.drawImage(img,ax,ay,30,30);
-}
-}
+                   ay=arrowY-10;
+                 ctx.drawImage(img,ax,ay,30,30);
+                   }
+           }
 
-}
- function drawman()   
+            }//to move archer up and down
+                function drawman()   
     
- {
+              {
 
- ctx.drawImage(img,ax,ay,30,30);
-  }
-function drawheart()
-{
-  ctx.drawImage(heart,heartx,hearty,5,5);
-}
- function drawmale()
- {
-   ctx.drawImage(male,malex,maley,5,5)
-}
- function gamepause(e)
-{
-  if(e.keyCode===80)
-  {
-    if(Paused)  
-       Paused=false;
-    else
-      Paused=true;
-  }
-}
+                 ctx.drawImage(img,ax,ay,30,30);
+               }
+              function drawheart()
+              {
+                ctx.drawImage(heart,heartx,hearty,5,5);
+               }
+               function drawmale()
+               {
+                  ctx.drawImage(male,malex,maley,5,5)
+                }
+               function gamepause(e)
+                {
+                    if(e.keyCode===80)
+                      {
+                          if(Paused)  
+                           Paused=false;
+                         else
+                          Paused=true;
+                     }
+                       }
 
                 
-
-var interval=setInterval(game,100);
+         var interval=setInterval(game,100);//set interval to make the movements happen continuously
 
 
 
